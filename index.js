3283144c6aa96047c40aba882fdaf17fec8aa2ee
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-21 18:29:21 +0700

    Make mysql work

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
-console.log('sqlite')
 require('dotenv').config()
 const express = require('express')
-const sqlite = require('sqlite')
+const mysql = require('mysql')
 
 const app = express()
 const port = process.env.PORT || 8000
-const dbPromise = sqlite.open(process.env.DB_PATH, { Promise })
 
-app.get('/users', async (req, res) => {
-  const db = await dbPromise
-  const users = await db.all('SELECT * FROM users')
-  if (users) {
-    res.send({
-      users: users,
-    })
-  }
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.DB_PWD,
+  database: 'mytasks',
 })
 
-app.get('/users/:id', async (req, res) => {
-  const db = await dbPromise
-  const user = await db.all('SELECT * FROM users WHERE id = ?', req.params.id)
-  if (user) {
-    res.send({
-      user: user,
-    })
+db.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+    return
   }
+
+  console.log('connected as id ' + db.threadId)
+})
+
+app.get('/users', (req, res) => {
+  let sql = 'SELECT * FROM users'
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err
+    console.log(results)
+  })
+})
+
+// db.end();
+
+// db.query('SELECT * FROM users', function(error, results, fields) {
+//   // When done with the db, release it.
+//   console.log(results)
+
+//   console.log('************')
+//   // Handle error after the release.
+//   if (error) throw error
+
+//   // Don't use the db here, it has been returned to the pool.
+// })
+
+// db.end(() => {
+//   console.log('end')
+// })
+
+app.get('/users', (req, res) => {
+  res.send({
+    message: 'hello',
+  })
 })
 
 app.listen(port)
